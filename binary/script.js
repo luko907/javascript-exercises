0f6ff6cc3bee099686bb3ed8bcb9e99faea01d51
Lucas Koch <luko907@hotmail.com>
2021-10-12 10:03:23 -0300

    binarytodecimal function

--- binary/script.js
+++ binary/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
-function BinaryToDecimal(num) {}
+function BinaryToDecimal(num) {
+  const arr = num.split("");
+  const pos = arr.length - 1;
+  let decimal = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    decimal += arr[i] * 2 ** pos;
+    pos--;
+  }
+  return decimal;
+}
 
 function DecimalToBinary(num) {}
