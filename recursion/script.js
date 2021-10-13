3244cd47d775d453368bc395790dca98c3a2fd92
Lucas Koch <luko907@hotmail.com>
2021-10-13 10:05:10 -0300

    factorial function

--- recursion/script.js
+++ recursion/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * nFactorial(n - 1);
+}
 
 function nFibonacci(n) {
   // Sequence Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,…
